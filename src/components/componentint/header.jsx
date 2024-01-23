c7e051a3d3cf67258073725914932d9c9e03bc67
Evan F <evanfd05@gmail.com>
2024-01-23 10:49:04 -0700

    Most file structuring is done

---
+++ src/components/componentint/header.jsx
@@ -0,0 +1,7 @@
+export default function Header() {
+    return(
+        <header>
+            <h2>Evan Fodemski</h2>
+        </header>
+    );
+}
---
+++ src/components/componentint/navbar.jsx
@@ -0,0 +1,10 @@
+export default function Nav({ links }) {
+    return(
+        <nav>
+            <ul classname="navBar">
+                
+
+            </ul>
+        </nav>
+    )
+}
