d634ac230e1818a5ca4382905237e01084a233ba
Evan F <evanfd05@gmail.com>
2024-04-30 21:25:47 -0600

    working changes

--- src/components/port.jsx
+++ src/components/port.jsx
@@ -213,8 +213,10 @@ export default function Portfolio() {
                                 <p className="innerdescport">
                                     A ReadMe generator created with Node, used on locall installation.                                </p>
                                 <div className="gitsvg">
-                                    
+
                                     {gitsvg}
+
+
 
 
                                 </div>
