fdf63c8a9d3ff2580021b7797d423f00ad9de65c
Evan F <evanfd05@gmail.com>
2024-04-30 18:31:34 -0600

    finished map rework and other small bug

--- src/components/aboutMe.jsx
+++ src/components/aboutMe.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import "../styles/about.css";
 import { useState } from "react";
-import { MapContainer, TileLayer, Marker, Popup, useMap } from 'react-leaflet'
+import { MapContainer, TileLayer, SVGOverlay, Marker, Popup, useMap } from 'react-leaflet'
 import 'leaflet/dist/leaflet.css'
 
 
@@ -10,21 +10,57 @@ export default function About() {
 
   const position = [38.8176, -104.8321]
 
+  var greenIcon = new L.Icon({
+    iconUrl: 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-green.png',
+    shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
+    iconSize: [28, 46],
+    iconAnchor: [12, 41],
+    popupAnchor: [1, -34],
+    shadowSize: [51, 51]
+  });
+
+
   return (
     <div className="aboutcontainer">
-      
+
+      <div className="topheadabt">
+        <h1 className="abth1">
+          About Me
+        </h1>
+      </div>
+
       <div >
-        <MapContainer className="mapcontainer" center={position} zoom={11.5} scrollWheelZoom={false}>
+        <MapContainer className="mapcontainer" center={position} zoom={12} scrollWheelZoom={false}>
           <TileLayer
             attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
             url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
           />
-          <Marker position={position}>
-            <Popup>
-              A pretty CSS3 popup. <br /> Easily customizable.
-            </Popup>
+          <Marker icon={greenIcon} position={position}>
+
+
+
           </Marker>
-        </MapContainer>,
+        </MapContainer>
+      </div>
+
+      <div className="abtwritingcon">
+
+
+        <div className="abtwriting">
+  
+          <p className="p1">
+            I'm Evan Fodemski, and let me share my coding journey. Graduating high school in Colorado Springs,
+            I ventured into the Denver University full-stack coding bootcamp that summer. The challenge and creativity instantly hooked me,
+            turning coding from a curiosity into a genuine passion. It became the medium through which I could bring my ideas to life.
+            Now equipped with a solid coding foundation, I'm determined to turn this passion into a fulfilling career.
+            From high school grad to coding enthusiast, I'm on a path to make my mark in the ever-evolving tech world.
+          </p>
+          <br />
+
+        </div>
+
+
+
       </div>
 
     </div>
@@ -33,16 +69,3 @@ export default function About() {
 }
 
 
-
-{/* <div className="abtwriting">
-        <h1>About Evan Fodemski</h1>
-        <p className="p1">
-          Hey, I'm Evan Fodemski, and let me share my coding journey. Graduating high school in Colorado Springs,
-          I ventured into the Denver University full-stack coding bootcamp that summer. The challenge and creativity instantly hooked me,
-          turning coding from a curiosity into a genuine passion. It became the medium through which I could bring my ideas to life.
-          Now equipped with a solid coding foundation, I'm determined to turn this passion into a fulfilling career.
-          From high school grad to coding enthusiast, I'm on a path to make my mark in the ever-evolving tech world.
-        </p>
-        <br />
-
-      </div> */}
